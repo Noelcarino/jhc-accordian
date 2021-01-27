a64d327e21a6b777d2ae0b8cd6e69abb724d3663
Noelcarino <leononirac1992@gmail.com>
2021-01-27 06:56:08 +0000

    1/26 | Box Contents now disappear when title box is clicked

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+document.getElementById('box-1').addEventListener('click', toggleBox);
+
+
+
+
+
+function toggleBox(){
+
+    if (document.getElementById('box-contents-1').style.display === 'block'){
+        document.getElementById('box-contents-1').style.display = 'none';
+    } else {
+        document.getElementById("box-contents-1").style.display = "block";
+    }
+
+}
