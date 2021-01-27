cb0af1f1ad4b9b902cca72f7da1635a6ad48ded9
Noelcarino <leononirac1992@gmail.com>
2021-01-27 07:58:50 +0000

    1/26 | All contents are now hidden when main box is clicked

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-document.getElementById('box-1').addEventListener('click', toggleBox);
+document.getElementById('box-1').addEventListener('click', toggleBox);document.getElementById('box-2').addEventListener('click', toggleBox);
+document.getElementById('box-3').addEventListener('click', toggleBox);
 
 
 
 
 
-function toggleBox(){
 
-    if (document.getElementById('box-contents-1').style.display === 'block'){
-        document.getElementById('box-contents-1').style.display = 'none';
-    } else {
-        document.getElementById("box-contents-1").style.display = "block";
+function toggleBox(e){
+
+    if (e.target.id === 'box-1') {
+        if (document.getElementById('box-contents-1').style.display === 'block') {
+            document.getElementById('box-contents-1').style.display = 'none';
+        } else {
+            document.getElementById('box-contents-1').style.display = "block";
+        }
+        return;
     }
-
+    if (e.target.id === 'box-2') {
+        if (document.getElementById('box-contents-2').style.display === 'block') {
+            document.getElementById('box-contents-2').style.display = 'none';
+        } else {
+            document.getElementById('box-contents-2').style.display = "block";
+        }
+        return;
+    }
+    if (e.target.id === 'box-3') {
+        if (document.getElementById('box-contents-3').style.display === 'block') {
+            document.getElementById('box-contents-3').style.display = 'none';
+        } else {
+            document.getElementById('box-contents-3').style.display = "block";
+        }
+        return;
+    }
 }
